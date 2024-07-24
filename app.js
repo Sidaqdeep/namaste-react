e6e28aa52b04957b7946e41c53a3e57c2d02cfe6
Sidaqdeep <behalsidaqdeepsingh@gmail.com>
2024-07-24 16:15:57 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1"),
+    React.createElement("h2", {}, "I am an h2"),
+  ])
+);
+
+// const heading = React.createElement("h1",{id:"heading"},"Hello world from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
